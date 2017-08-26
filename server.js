c22f8aba5be4c34f6511a843ddcd572669b2004c
selvadevan <selvadevan@gmail.com>
2017-08-26 17:04:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/art1', function (req, res) {
+  res.send('Article 1 served');
+});
+
+app.get('/art2', function (req, res) {
+  res.send('Article 2 served');
+});
+
+app.get('/art3', function (req, res) {
+  res.send('Article 3 served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
